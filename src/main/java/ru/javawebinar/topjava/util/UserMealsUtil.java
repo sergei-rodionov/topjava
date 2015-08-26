6fc266ae96e0c9ef2f2db4fba2df866f32facb29
Sergei Rodionov <mail@sergeirodionov.ru>
2015-08-26 21:39:11 +0300

    HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,14 +3,12 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -36,25 +34,26 @@ public class UserMealsUtil {
 
         List<UserMealWithExceed> result = new LinkedList();
 
-        // создаем Map по дням
-        Map<LocalDate, List<UserMeal>> mapByDate = mealList.stream()
-                .collect(Collectors.groupingBy(u -> u.getDateTime().toLocalDate()
-                ));
-        // перебираем дни
-        for (Map.Entry m : mapByDate.entrySet()) {
-            // проверяем был перебор по каллориям или нет
-            boolean exceed = ((List<UserMeal>) m.getValue()).stream()
-                    .collect(Collectors.summarizingInt(UserMeal::getCalories))
-                    .getSum() > caloriesPerDay;
-            // если находим записи с требуемым временем то добавляем в результурующий лист
-            for (UserMeal meal : ((List<UserMeal>) m.getValue()).stream()
-                                  .filter(u->(u.getDateTime().toLocalTime().isAfter(startTime)
-                                           && u.getDateTime().toLocalTime().isBefore(endTime)))
-                                  .collect(Collectors.toList())  ) {
 
-                result.add(new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(), exceed));
-            }
-        }
+        mealList.stream()
+                // создаем Map по дням
+                .collect(Collectors.groupingBy(u -> u.getDateTime().toLocalDate())).entrySet().stream()
+
+                // перебираем дни
+                .forEach(days -> {
+
+                    // вычисляем один раз был перебор по каллориям или нет
+                    boolean exceed = days.getValue().stream()
+                            .collect(Collectors.summarizingInt(UserMeal::getCalories)).getSum() > caloriesPerDay;
+
+                    // если находим записи с требуемым временем то добавляем в результурующий лист
+                    days.getValue().stream()
+                            .filter(timeMeal -> (timeMeal.getDateTime().toLocalTime().isAfter(startTime)
+                                    && timeMeal.getDateTime().toLocalTime().isBefore(endTime)))
+                            .forEach(meal ->
+                                    result.add(new UserMealWithExceed(meal.getDateTime(),
+                                            meal.getDescription(), meal.getCalories(), exceed)));
+                });
 
         return result;
     }
