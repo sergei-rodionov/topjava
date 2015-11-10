999d4b9c3bde83bd5fba1addc4c54f25b0b9a5c1
Sergei Rodionov <mail@sergeirodionov.ru>
2015-11-10 14:16:01 +0300

    HW09

--- src/test/java/ru/javawebinar/topjava/web/meal/UserMealControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/meal/UserMealControllerTest.java
@@ -1,9 +1,13 @@
 package ru.javawebinar.topjava.web.meal;
 
 import org.junit.Test;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import ru.javawebinar.topjava.web.AbstractControllerTest;
 
 import static org.hamcrest.Matchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -18,6 +22,10 @@ public class UserMealControllerTest extends AbstractControllerTest {
 
     @Test
     public void testMealList() throws Exception {
+        // Login as viewer
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken("user@yandex.ru", "password"));
+
         mockMvc.perform(get("/meals"))
                 .andDo(print())
                 .andExpect(status().isOk())
@@ -30,5 +38,15 @@ public class UserMealControllerTest extends AbstractControllerTest {
                                 hasProperty("description", is(MEAL1.getDescription()))
                         )
                 )));
+
+        SecurityContextHolder.clearContext();
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        mockMvc.perform(formLogin("/spring_security_check").user("user@yandex.ru").password("password"))
+                .andDo(print())
+                .andExpect(authenticated())
+                .andExpect(redirectedUrl("/meals"));
     }
 }
